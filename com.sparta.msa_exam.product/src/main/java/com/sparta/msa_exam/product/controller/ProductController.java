28632f1269298c8a18654f1b99c6c1544a5a818c
seeyoungryu <seayeux@gmail.com>
2024-08-07 11:32:05 +0900

    pack.product 컨트롤러 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
@@ -1,4 +1,35 @@
 package com.sparta.msa_exam.product.controller;
 
+import com.sparta.msa_exam.product.dto.ProductRequestDTO;
+import com.sparta.msa_exam.product.dto.ProductResponseDTO;
+import com.sparta.msa_exam.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> addProduct(@RequestBody ProductRequestDTO productRequestDTO) {
+        ProductResponseDTO createdProduct = productService.addProduct(productRequestDTO);
+        return ResponseEntity.ok(createdProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getProducts() {
+        List<ProductResponseDTO> products = productService.getProducts();
+        return ResponseEntity.ok(products);
+    }
 }
