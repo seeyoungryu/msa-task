1b7b3d7cf5046b640babe077c920d108cc819d1a
seeyoungryu <seayeux@gmail.com>
2024-08-07 09:22:03 +0900

    add @EnableFeignClients

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/OrderApplication.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/OrderApplication.java
@@ -2,7 +2,9 @@ package com.sparta.msa_exam.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class OrderApplication {
 
--- com.sparta.msa_exam.server/src/test/java/com/sparta/msa_exam/server/ServerApplicationTests.java
+++ com.sparta.msa_exam.server/src/test/java/com/sparta/msa_exam/server/ServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.sparta.msa_exam.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootTest
 class ServerApplicationTests {
 
