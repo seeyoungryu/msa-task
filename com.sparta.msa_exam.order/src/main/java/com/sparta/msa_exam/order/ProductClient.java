bc626b2034c7bb5b2134a9090f49c5d9c7ae6b8c
seeyoungryu <seayeux@gmail.com>
2024-08-08 00:04:12 +0900

    add ProductClient(Feign)

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/ProductClient.java
@@ -0,0 +1,12 @@
+package com.sparta.msa_exam.order;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "product-service")
+public interface ProductClient {
+    @GetMapping("/product/{id}")
+    String getProduct(@PathVariable("id") String id);
+}
+
