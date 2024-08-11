cfbf9901a70f77c2c9c01ffadcfecb8c8a218dc4
seeyoungryu <seayeux@gmail.com>
2024-08-11 18:34:07 +0900

    응답헤더 로드밸런싱 포트 확인 완료

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.sparta.msa_exam.product.controller;
 import com.sparta.msa_exam.product.dto.ProductRequestDTO;
 import com.sparta.msa_exam.product.dto.ProductResponseDTO;
 import com.sparta.msa_exam.product.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+
     @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받음
     private String serverPort;
 
@@ -30,7 +32,9 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductResponseDTO>> getProducts() {
+    public ResponseEntity<List<ProductResponseDTO>> getProducts(HttpServletResponse response) {
+        // 응답 헤더에 서버 포트 번호를 설정
+        response.setHeader("Server-Port", serverPort);
         List<ProductResponseDTO> products = productService.getProducts();
         return ResponseEntity.ok(products);
     }
